0d2f3da4e733e97c1368f5109a014f7f36fd6009
Rajat <rajat.sudagade@gmail.com>
2020-02-24 20:13:39 -0600

    second commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const handleData = {
-  
   display_data: function (resJSON) {
     const tableTemplate = $("#table-template").html()
     const template = Handlebars.compile(tableTemplate)
@@ -7,24 +6,14 @@ const handleData = {
     $("#table-container").html(tableData)
   },
 
-  loadData: function (url, dataType) {
+  loadData: function (url) {
     $.ajax({
       url: url,
-      type: "GET",
-      crossDomain: true,
-      dataType: dataType,
-      success: this.display_data,
-      error: function() {
-        alert("Request has failed because of CORB. While I am trying to find a solution for this new web feature, please use the other button to load table data")
-      }
-    })
+      type: 'GET',
+    }).then(this.display_data)
   }
 }
 
-$(".load-from-api").click(() => { 
-  handleData.loadData("http://aimtell.com/files/sites.json", 'jsonp')
+$(".btn").click(() => { 
+  handleData.loadData("https://aimtell.com/files/sites.json")
 });
-
-$(".load-offline").click(() => {
-  handleData.loadData("/data/offlineData.json", 'json')
-})
